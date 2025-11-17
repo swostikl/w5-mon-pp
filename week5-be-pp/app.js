92276f524596bbaf323875e1aa419764a3c5a8a9
Lee <178769024+RyougiLee@users.noreply.github.com>
2025-11-17 15:02:50 +0200

    iteration 5 done

--- week5-be-pp/app.js
+++ week5-be-pp/app.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
-const { unknownEndpoint } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
+const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
 require('dotenv').config();
-
 
 const morgan = require("morgan");
 
@@ -22,8 +21,15 @@ app.use("/api/tours", tourRouter);
 // Use the userRouter for all /users routes
 app.use("/api/users", userRouter);
 
+// Example route that throws an error
+app.get('/error', (req, res, next) => {
+  // Trigger an error
+  const error = new Error("Network problem");
+  next(error);
+});
+
 app.use(unknownEndpoint);
-// app.use(errorHandler);
+app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
 // Start the server
